216047e57134872edfbac814b8d0d38604030e31
Aurel-Dragos Hofnar <aurel-dragos@hofnar.ro>
2017-12-12 13:00:22 +0200

    day12

---
+++ day12/day12-1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+groups = [[0]]
+
+def addtToGroups(connected):
+	global groups
+	tmp = []
+	for group in groups:
+		for element in connected:
+			if element in group:
+				tmp += [group]
+			
+	for delete in tmp:
+		if delete in groups:
+			groups.remove(delete)
+
+	groups += [list(set(sum(tmp, []) + connected))]
+
+lines = [line.rstrip('\n') for line in open('input')]
+
+for line in lines:
+	
+	src = int(line.split("<->")[0])
+	dst = map(int, line.split("<->")[1].split(","))
+	# connected ends
+	connected = list(set([src] + dst))
+	addtToGroups(connected)
+
+for x in groups:
+	if 0 in x:
+		print len(x)
